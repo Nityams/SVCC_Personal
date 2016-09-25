821ade5af5382f088223f547eb6b2b42d213a35a
Nityams <nityamshrestha@gmail.com>
2016-09-25 01:46:02 -0700

    cmt2

--- FirstGen/app/src/main/java/teamrocket/firstgen/IntroPage.java
+++ FirstGen/app/src/main/java/teamrocket/firstgen/IntroPage.java
@@ -2,6 +2,9 @@ package teamrocket.firstgen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class IntroPage extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class IntroPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_page);
     }
+    public void onClickSignup(View view){
+        Intent intent = new Intent(this,ProfilePage.class );
+        startActivity(intent);
+        Toast.makeText(this, "Signup clicked", Toast.LENGTH_LONG).show();
+    }
+    public void onClickLogin(View view){
+        Intent intent = new Intent(this,LoginPage.class );
+        startActivity(intent);
+        Toast.makeText(this, "Login clicked", Toast.LENGTH_LONG).show();
+
+    }
+
 }
--- FirstGen/app/src/main/java/teamrocket/firstgen/ProfilePage.java
+++ FirstGen/app/src/main/java/teamrocket/firstgen/ProfilePage.java
@@ -2,12 +2,18 @@ package teamrocket.firstgen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ProfilePage extends AppCompatActivity {
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        img = (ImageView) findViewById(R.id.profile_pic);
+
+
     }
 }
