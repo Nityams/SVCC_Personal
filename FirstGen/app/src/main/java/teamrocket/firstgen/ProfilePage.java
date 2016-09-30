361f6fa42f5ea366dece4f868b8cfee85e8873e8
Rabia Tugce Keser <89904.keser@students.itu.edu>
2016-09-30 15:29:06 -0700

    Hackathon Project

--- FirstGen/app/src/main/java/teamrocket/firstgen/ProfilePage.java
+++ FirstGen/app/src/main/java/teamrocket/firstgen/ProfilePage.java
@@ -2,18 +2,53 @@ package teamrocket.firstgen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfilePage extends AppCompatActivity {
-    private ImageView img;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);
+        private EditText f_name, l_name, city, email, major, ziptxt, gpatxt;
+        private int zipcode;
+        private double gpa;
 
-        img = (ImageView) findViewById(R.id.profile_pic);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_profile_page);
 
+       /* f_name = (EditText) findViewById(R.id.first_name);
+        l_name = (EditText) findViewById(R.id.last_name);
+        city = (EditText) findViewById(R.id.city_name);
+        email = (EditText) findViewById(R.id.email);
+        major = (EditText) findViewById(R.id.major);
 
-    }
+        ziptxt = (EditText) findViewById(R.id.zip_code);
+        zipcode = (Integer.parseInt(ziptxt.getText().toString()));
+        gpatxt = (EditText) findViewById(R.id.gpa);
+        gpa = (Double.parseDouble(gpatxt.getText().toString()));*/
+        Button btnSave= (Button) findViewById(R.id.profile_save);
+        btnSave.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                        finish();
+                }
+        });
+        Button btnCancel= (Button) findViewById(R.id.profile_cancel);
+        btnSave.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                        finish();
+                }
+        });
+
+        }
+
+        /*public void onClickCancel(View view) {
+            // cancel current input and go back to the previous data
+        }
+
+        public void onClickSave(View view) {
+            // save current input
+        }*/
 }
